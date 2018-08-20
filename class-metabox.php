3915d71f59773535c8b3f336785589759ba75242
Marek Aleszczyk <marek@e-kreatywnie.pl>
2018-08-20 14:23:33 +0200

    -

--- class-metabox.php
+++ class-metabox.php
@@ -1,4 +1,5 @@
 <?php
+/* Version: 1.2 */
 
 defined( 'ABSPATH' ) or exit();
 
@@ -34,13 +35,25 @@ if ( !class_exists( 'wpMetabox' ) && trait_exists( 'wpAutoHooks' ) ) :
 			return $priority;
 		}
 		
+		public static function get_context() {
+			return !in_array( static::CONTEXT, static::CONTEXTS ) ? 
+							static::DEFAULT_CONTEXT : static::CONTEXT;
+		}
+		
+		public static function get_title() {
+			return sprintf( 
+								__( static::TITLE, 'marale-wp-connections' ), 
+								self::class.'::TITLE', 
+								__( 'constant', 'marale-wp-connections' ),
+								static::class
+							);
+		}
+		
 		public static function add() {
 			
 			if ( !is_admin() ) { return; }
 			
 			self::static_connect();
-			
-			$_ = __( 'Please, extend %s %s in a %s class.', 'marale-wp-connections' );
 		}
 		
 		public static function add_meta_box() {
@@ -53,21 +66,11 @@ if ( !class_exists( 'wpMetabox' ) && trait_exists( 'wpAutoHooks' ) ) :
 			foreach ( static::SCREENS as $screen ) {
 				add_meta_box(
 						static::ID,    // Unique ID
-						
-						sprintf( 
-								__( static::TITLE, 'marale-wp-connections' ), 
-								self::class.'::TITLE', 
-								__( 'constant', 'marale-wp-connections' ),
-								static::class
-							),          // Box title
-						
+						static::get_title(),          // Box title
 						static::class . '::html',   // Content callback
-						
 						$screen,		// Post type
-						
-						!in_array( static::CONTEXT, static::CONTEXTS ) ? 
-							static::DEFAULT_CONTEXT : static::CONTEXT,
-						self::get_priority()
+						static::get_context(),
+						static::get_priority()
 					);
 			}
 			
@@ -87,11 +90,16 @@ if ( !class_exists( 'wpMetabox' ) && trait_exists( 'wpAutoHooks' ) ) :
 			self::hook_check(__FUNCTION__);
 			static::add_meta_box();
 		}
+		
+		public static function shutdown_wpaction() {
+			__( 'Please, extend %s %s in a %s class.', 'marale-wp-connections' );
+		}
 	}
 	
 	abstract class wpDashboardMetabox extends wpMetabox {
 		use wpAutoHooks;
 		
+		const ID = 'dashboard-metabox';
 		const DEFAULT_CONTEXT = 'normal';
 		const CONTEXTS = [ self::DEFAULT_CONTEXT, 'side' ];
 		const SCREENS  = ['dashboard'];
@@ -107,7 +115,12 @@ if ( !class_exists( 'wpMetabox' ) && trait_exists( 'wpAutoHooks' ) ) :
 		
 		public static function wp_dashboard_setup_wpaction() {
 			self::hook_check(__FUNCTION__);
-			static::add_meta_box();
+                        
+			if ( static::get_context() === self::DEFAULT_CONTEXT ) {
+				wp_add_dashboard_widget(static::ID, static::get_title(), [static::class, 'html'] );
+			} else {
+				static::add_meta_box();
+			}
 		}
 	}	
 	
